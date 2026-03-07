0471df3e1615afbd7e71a117c938e1f893e22c01
Nishizawa Motoki <89074451+nishizawamotoki@users.noreply.github.com>
2026-03-07 06:06:17 +0000

    ユーザーの一覧・詳細ページを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:index, :show]
+
   root to: "books#index"
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

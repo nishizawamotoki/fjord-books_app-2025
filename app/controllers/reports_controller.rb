dd9145c8575ae70ea0828aade81e4a155e6d556c
nishizawamotoki <sokomichi.hizawa@gmail.com>
2026-05-01 15:21:59 +0900

    自動生成された不要な機能を削除した

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -26,27 +26,19 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.build(report_params)
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to @report, notice: "Report was successfully created." }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to @report, notice: "Report was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: "Report was successfully updated." }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to @report, notice: "Report was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -54,10 +46,7 @@ class ReportsController < ApplicationController
   def destroy
     @report.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to reports_path, status: :see_other, notice: "Report was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to reports_path, status: :see_other, notice: "Report was successfully destroyed."
   end
 
   private

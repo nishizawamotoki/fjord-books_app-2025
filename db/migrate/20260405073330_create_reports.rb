7d2e9cad7223c1f595dcf2429d0e9d059420520a
nishizawamotoki <sokomichi.hizawa@gmail.com>
2026-05-07 17:16:19 +0900

    マイグレーションファイルを集約した

--- db/migrate/20260405073330_create_reports.rb
+++ db/migrate/20260405073330_create_reports.rb
@@ -1,8 +1,9 @@
 class CreateReports < ActiveRecord::Migration[8.0]
   def change
     create_table :reports do |t|
-      t.string :title
-      t.text :content
+      t.string :title, null: false
+      t.text :content, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
